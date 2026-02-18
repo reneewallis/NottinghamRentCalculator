b4ee34f06ef481daaffd7e63850d5fa22a9fe802
Renee Wallis <reneewallis1@hotmail.co.uk>
2026-02-18 19:24:11 +0000

    fixed screen size

--- rent-calculator/src/app/page.tsx
+++ rent-calculator/src/app/page.tsx
@@ -4,7 +4,7 @@ import Tabs from "@/src/components/Tabs/Tabs";
 
 export default function Home() {
   return (
-  <div className="min-h-screen bg-gray-500 p-8 text-gray-50">
+  <div className="w-screen h-screen bg-gray-500 p-8 text-gray-50">
     <Header></Header>
     <Tabs></Tabs>
   </div>
