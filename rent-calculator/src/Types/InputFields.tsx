6861fab20f918494cdadf4225877bdf4173725e2
Renee Wallis <reneewallis1@hotmail.co.uk>
2026-03-18 22:44:46 +0000

    refactored types and added validation to textbox

---
+++ rent-calculator/src/Types/InputFields.tsx
@@ -0,0 +1,42 @@
+import { ChangeEvent } from "react";
+import { Dayjs } from "dayjs"
+
+export type Alignment = "left" | "center" | "right";
+
+type CommonTextBoxProps = {
+    label:string;
+    text: string;
+    alignment?: Alignment;
+    width?: number;
+    valid?: boolean;
+}
+
+export type InputTextBoxProps = CommonTextBoxProps & {
+    readOnly: false;
+    onChange: (e:ChangeEvent<HTMLTextAreaElement>) => void; 
+}
+
+export type ReadOnlyTextBoxProps = CommonTextBoxProps & {
+    readOnly: true;
+}
+
+export type TextBoxProps = InputTextBoxProps | ReadOnlyTextBoxProps;
+
+type CommonDateBoxProps = {
+    label:string;
+    alignment?:Alignment;
+    minDate?:Dayjs;
+    onChange?: (value:Dayjs) => void;
+}
+
+type UncontrolledDateBoxProps = CommonDateBoxProps & {
+    controlled?: false;
+};
+
+type ControlledDateBoxProps = CommonDateBoxProps & {
+    controlled: true;
+    value: Dayjs | null;
+    setValue: (value:Dayjs|null) => void;
+}
+
+export type DateBoxProps = UncontrolledDateBoxProps | ControlledDateBoxProps;
