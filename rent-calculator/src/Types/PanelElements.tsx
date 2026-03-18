ff45cedb29c179b409777701a284f237e276dd9e
Renee Wallis <reneewallis1@hotmail.co.uk>
2026-03-18 22:44:13 +0000

    added installment scroller and calculator panel prop types

--- rent-calculator/src/Types/PanelElements.tsx
+++ rent-calculator/src/Types/PanelElements.tsx
@@ -1,8 +1,29 @@
+import { ReactElement } from "react";
 import { DropdownBoxProps } from "./Dropdown";
-import { InputTextBoxProps, ReadOnlyTextBoxProps} from "./TextBox";
+import { DateBoxProps, InputTextBoxProps, ReadOnlyTextBoxProps, TextBoxProps} from "./InputFields";
+import { InstallmentFrequency } from "./RentCalculator";
 
 export type CalculatorBoxProps = {
     dropDownProps: DropdownBoxProps;
     inputTextBoxProps: InputTextBoxProps; 
     resultTextBoxes: ReadOnlyTextBoxProps[];
+    flipResultTextBoxes?: boolean;
+}
+
+export type InstallementScrollerProps = {
+    totalInstallments: number;
+    installmentNumber: number;
+    frequency: InstallmentFrequency;
+    onChange?: (installment:number) => void
+}
+
+type MainPanelElement = ReactElement<TextBoxProps> | ReactElement<DateBoxProps> | ReactElement<InstallementScrollerProps>;
+type SidePanelElement = ReactElement<TextBoxProps>| ReactElement<DropdownBoxProps>;
+
+export type CalculatorPanelProps = {
+    circleValue: string;
+    circleLabel: string;
+    mainPanelBoxes: MainPanelElement[];
+    sidePanelBoxes: SidePanelElement[];
+    flipPanel?: boolean;
 }
