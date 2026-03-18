16451cff5700750f3bce6be86bb5bdfb08f4f123
Renee Wallis <reneewallis1@hotmail.co.uk>
2026-03-18 22:33:46 +0000

    fixed screen border issues

--- rent-calculator/src/components/Header/Header.tsx
+++ rent-calculator/src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import { HeaderProps } from "@/src/Types/Header";
 
 function Header({children}:HeaderProps){
     return(
-        <div className="max-w-full">
+        <div>
             <h1 className="text-3xl font-bold mb-2">Rent Calculator</h1>
             {children}
         </div>
