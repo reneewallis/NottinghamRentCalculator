44b39f1484e66ffba35959d1289e6d8f7d7ba585
Renee Wallis <reneewallis1@hotmail.co.uk>
2026-03-18 22:32:33 +0000

    fixed arrow button

---
+++ rent-calculator/src/components/Buttons/ArrowButton.tsx
@@ -0,0 +1,24 @@
+import { ArrowButtonProps } from "@/src/Types/Buttons";
+import React from "react";
+
+function ArrowButton({direction, onClick}:ArrowButtonProps){
+    return(
+        <button className="flex justify-center items-center border-3 rounded-full cursor-pointer border-gray-200 text-gray-200 hover:border-gray-50 hover:text-gray-50 p-1" onClick={onClick}>
+            {direction === "left"?
+                <div className="-translate-x-px">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+                    </svg>
+                </div>
+                :
+                <div className="translate-x-px">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                    </svg>
+                </div>
+            }
+        </button>
+    )
+}
+
+export default ArrowButton;
