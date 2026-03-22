b2c4615b4da22969471f6d71e9e41d4e896fa75a
Renee <reneewallis1@hotmail.co.uk>
2026-03-22 18:18:57 +0000

    changed empty tab state to render and be hidden to stop rerendering

--- rent-calculator/src/components/Tabs/Tabs.tsx
+++ rent-calculator/src/components/Tabs/Tabs.tsx
@@ -166,21 +166,17 @@ export default function Tabs(){
             </div>
             }
         </div>
-        {tabs.wrappedTabArr.length === 0?(
-            <div className="flex flex-col items-center mt-20 mx-auto w-fit h-fit p-10 bg-gray-800 border-solid border-gray-200 border-3 inset-shadow-sm ring-2 ring-gray-100/60 rounded-2xl">
+        <div hidden={tabs.wrappedTabArr.length !== 0} className="flex flex-col items-center mt-20 mx-auto w-fit h-fit p-10 bg-gray-800 border-solid border-gray-200 border-3 inset-shadow-sm ring-2 ring-gray-100/60 rounded-2xl">
                 <h1 className="font-bold text-center whitespace-nowrap text-4xl text-gray-50 text-shadow-lg/20 pt-10">Welcome to Nottigham&#39;s Rent Calculator</h1>
                 <button key={"startNewTabButton"} onClick={()=>{dispatch({type: TabsActions.NEW_TAB})}} className="bg-gray-700 border-gray-50 text-gray-50 text-shadow-lg/20 text-xl border-2 inset-shadow-sm shadow-xl text-center mt-12 mb-12 p-6 rounded-full focus:outline-none transition-colors cursor-pointer duration-200 hover:text-fuchsia-700 hover:border-fuchsia-700">Create New Tab</button>
-            </div>)
-            :(
-                tabs.wrappedTabArr.map((tab, index) => {
-                    // TO:DO => return panel for non Tab Tabs
-                    return(
-                        <div key={`tabChild${tab.id}`} hidden={tabs.activeTab !== index}>
-                            <TabPanel today={tab.today} todayString={tab.todayString}></TabPanel>
-                        </div>
-                    )
-                })
-            )}
+        </div>
+        {tabs.wrappedTabArr.map((tab, index) => {
+            return(
+                <div key={`tabChild${tab.id}`} hidden={tabs.activeTab !== index}>
+                    <TabPanel today={tab.today} todayString={tab.todayString}></TabPanel>
+                </div>
+            );
+        })}
     </div>
   );
 }
