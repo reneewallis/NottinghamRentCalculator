b712a792eb389745d01e82c60f66ae95b022cc00
Renee <reneewallis1@hotmail.co.uk>
2026-03-22 23:44:15 +0000

    found tree hydration issue, changed language location

--- rent-calculator/src/app/layout.tsx
+++ rent-calculator/src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en-GB">
       <body className={`${ubuntuFont.variable} antialiased`}>{children}</body>
     </html>
   );
