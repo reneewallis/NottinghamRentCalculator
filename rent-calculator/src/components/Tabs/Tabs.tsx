99e5a50b94aa2539beedd875cb236568467322ee
Renee <reneewallis1@hotmail.co.uk>
2026-04-01 00:10:35 +0100

    changed conditional rendering to different return statements

--- rent-calculator/src/components/Tabs/Tabs.tsx
+++ rent-calculator/src/components/Tabs/Tabs.tsx
@@ -19,6 +19,26 @@ export default function Tabs() {
     lastTabActive: false,
     wrappedTabArr: [],
   });
+
+  if (tabs.wrappedTabArr.length === 0) {
+    return (
+      <div className="flex flex-col items-center mt-20 mx-auto w-fit h-fit p-10 bg-gray-800 border-solid border-gray-200 border-3 inset-shadow-sm ring-2 ring-gray-100/60 rounded-2xl">
+        <h1 className="font-bold text-center whitespace-nowrap text-4xl text-gray-50 text-shadow-lg/20 pt-10">
+          Welcome to Nottigham&#39;s Rent Calculator
+        </h1>
+        <button
+          key={"startNewTabButton"}
+          onClick={() => {
+            dispatch({ type: TabsActions.NEW_TAB });
+          }}
+          className="bg-gray-700 border-gray-50 text-gray-50 text-shadow-lg/20 text-xl border-2 inset-shadow-sm shadow-xl text-center mt-12 mb-12 p-6 rounded-full focus:outline-none transition-colors cursor-pointer duration-200 hover:text-fuchsia-700 hover:border-fuchsia-700"
+        >
+          Create New Tab
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className="mt-2 w-full">
       <div className="flex flex-wrap items-center border-b border-gray-800">
@@ -63,51 +83,31 @@ export default function Tabs() {
               </div>
             ),
         )}
-
-        {tabs.wrappedTabArr.length > 0 && (
-          <div
-            className={`flex items-center mx-0.5 h-7 mt-3 transition-colors duration-100 ${tabs.activeTab === tabs.wrappedTabArr.length - 1 || tabs.hoverLast === true ? "border-gray-500" : "border-l-gray-800 border-l-2"}`}
-          >
-            <NewTabButton
-              onClick={() => {
-                dispatch({ type: TabsActions.NEW_TAB });
-              }}
-            ></NewTabButton>
-            <HistoryButton
-              showHistory={tabs.showHistory}
-              onClick={() => {
-                dispatch({ type: TabsActions.VIEW_HISTORY });
-              }}
-            ></HistoryButton>
-            <DropdownMenu
-              items={[
-                {
-                  label: MenuItems.CLOSE_ALL_TABS,
-                  onClick: () => {
-                    dispatch({ type: TabsActions.CLOSE_ALL_TABS });
-                  },
-                },
-              ]}
-            ></DropdownMenu>
-          </div>
-        )}
-      </div>
-      <div
-        hidden={tabs.wrappedTabArr.length !== 0}
-        className="flex flex-col items-center mt-20 mx-auto w-fit h-fit p-10 bg-gray-800 border-solid border-gray-200 border-3 inset-shadow-sm ring-2 ring-gray-100/60 rounded-2xl"
-      >
-        <h1 className="font-bold text-center whitespace-nowrap text-4xl text-gray-50 text-shadow-lg/20 pt-10">
-          Welcome to Nottigham&#39;s Rent Calculator
-        </h1>
-        <button
-          key={"startNewTabButton"}
-          onClick={() => {
-            dispatch({ type: TabsActions.NEW_TAB });
-          }}
-          className="bg-gray-700 border-gray-50 text-gray-50 text-shadow-lg/20 text-xl border-2 inset-shadow-sm shadow-xl text-center mt-12 mb-12 p-6 rounded-full focus:outline-none transition-colors cursor-pointer duration-200 hover:text-fuchsia-700 hover:border-fuchsia-700"
+        <div
+          className={`flex items-center mx-0.5 h-7 mt-3 transition-colors duration-100 ${tabs.activeTab === tabs.wrappedTabArr.length - 1 || tabs.hoverLast === true ? "border-gray-500" : "border-l-gray-800 border-l-2"}`}
         >
-          Create New Tab
-        </button>
+          <NewTabButton
+            onClick={() => {
+              dispatch({ type: TabsActions.NEW_TAB });
+            }}
+          ></NewTabButton>
+          <HistoryButton
+            showHistory={tabs.showHistory}
+            onClick={() => {
+              dispatch({ type: TabsActions.VIEW_HISTORY });
+            }}
+          ></HistoryButton>
+          <DropdownMenu
+            items={[
+              {
+                label: MenuItems.CLOSE_ALL_TABS,
+                onClick: () => {
+                  dispatch({ type: TabsActions.CLOSE_ALL_TABS });
+                },
+              },
+            ]}
+          ></DropdownMenu>
+        </div>
       </div>
       {tabs.wrappedTabArr.map((tab, index) => {
         return (
