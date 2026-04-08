31c091442beb7f88ff1b466bdc22bb654dcd5b60
Renee <reneewallis1@hotmail.co.uk>
2026-04-08 20:53:01 +0100

    added console log for unfound session storage items

---
+++ rent-calculator/src/utils/SessionStorage/sessionStorage.ts
@@ -0,0 +1,24 @@
+export function getSessionStorageItem<T>(key: string): T | null {
+    try {
+        const item = sessionStorage.getItem(key);
+        if (item) {
+            return JSON.parse(item);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+    console.log(`${key} not found in session storage\n`);
+    return null;
+}
+
+export function setSessionStorageItem<T>(key: string, value: T) {
+    try {
+        sessionStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export function removeSessionStorageItem(key: string) {
+    sessionStorage.removeItem(key);
+}
