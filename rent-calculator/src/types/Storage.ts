023ffe706c9186f10179f24e697f9bb6909a7b43
Renee <reneewallis1@hotmail.co.uk>
2026-04-09 20:20:03 +0100

    added Storage types

---
+++ rent-calculator/src/types/Storage.ts
@@ -0,0 +1,19 @@
+import { Dayjs } from "dayjs";
+import { CalculatorState } from "./RentCalculator";
+import { TabsTabWrapper } from "./Tabs";
+
+type SerialisedDayjs<T> = {
+    [K in keyof T]: T[K] extends Dayjs
+        ? string
+        : T[K] extends Dayjs | null
+          ? string | null
+          : T[K];
+};
+
+type SerialisedCalculatorState = SerialisedDayjs<CalculatorState>;
+
+export type SerialisedTabsTabWrapper = {
+    [K in keyof TabsTabWrapper]: TabsTabWrapper[K] extends CalculatorState
+        ? SerialisedCalculatorState
+        : TabsTabWrapper[K];
+};
