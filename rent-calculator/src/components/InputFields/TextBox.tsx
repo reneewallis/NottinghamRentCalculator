e80b5c28deea822b1a2b4fc83037623bca73cf8c
renee <2560885w@student.gla.ac.uk>
2026-05-12 20:01:20 +0100

    removed right margin on textbox labels and added responsive text box width

--- rent-calculator/src/components/InputFields/TextBox.tsx
+++ rent-calculator/src/components/InputFields/TextBox.tsx
@@ -1,5 +1,6 @@
 import { TextBoxProps } from "../../types/InputFields";
 import React from "react";
+import { fluidCSSWidthScale } from "@/src/utils/HelperFunctions";
 
 function TextBox(props: TextBoxProps) {
     const {
@@ -28,13 +29,13 @@ function TextBox(props: TextBoxProps) {
     }
     return (
         <label
-            className={`inline-flex flex-col text-gray-200 text-2xl whitespace-nowrap ${alignmentString} focus-within:font-semibold hover:text-gray-100 focus-within:text-gray-50 transition-colors`}
+            className={`inline-flex flex-col text-gray-200 text-base md:text-xl lg:text-2xl whitespace-nowrap ${alignmentString} focus-within:font-semibold hover:text-gray-100 focus-within:text-gray-50 transition-colors`}
         >
-            <span className="w-fit h-fit mb-3 mr-1.5 cursor-pointer">
+            <span className="w-fit h-fit mb-1.25 md:mb-2 lg:mb-3 cursor-pointer">
                 {label}
             </span>
             <textarea
-                style={{ width: `${width}rem` }}
+                style={{ width: fluidCSSWidthScale(`${width*0.65}rem`,`${width}rem`,`${width*1.25}rem`)}}
                 className={`resize-none rounded-2xl py-2.5 px-3 border-2 font-normal ${valid ? "border-gray-300 hover:border-gray-100 focus:border-gray-50" : "border-red-600 hover:border-red-500 focus-within:border-red-500"} focus:outline-none transition-colors ${readOnly ? "bg-gray-800" : "bg-gray-700 hover:bg-gray-800 focus:bg-gray-900"}`}
                 value={text}
                 {...(!readOnly && { onChange: props.onChange })}
@@ -42,7 +43,6 @@ function TextBox(props: TextBoxProps) {
                 spellCheck={false}
                 autoComplete="off"
                 autoCorrect="off"
-                cols={14}
                 rows={1}
                 maxLength={8}
             ></textarea>
