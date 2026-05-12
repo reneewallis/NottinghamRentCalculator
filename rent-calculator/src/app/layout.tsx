ab9bf50c24f3c4709a25be2d8b9b7e5c83160b7d
renee <2560885w@student.gla.ac.uk>
2026-05-12 19:58:50 +0100

    added viewport tag for responsive design and made body background gray to fix a mobile issue

--- rent-calculator/src/app/layout.tsx
+++ rent-calculator/src/app/layout.tsx
@@ -19,8 +19,9 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en-GB" className="h-full">
-            <body className={`${ubuntuFont.variable} antialiased h-full`}>
+        <html lang="en-GB">
+            <head><meta name="viewport" content="width=device-width, initial-scale=1.0" /></head>
+            <body className={`${ubuntuFont.variable} antialiased bg-gray-500`}>
                 {children}
             </body>
         </html>
