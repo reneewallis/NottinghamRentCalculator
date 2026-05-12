41b1b687d231d9bd86412bc518a0a4643b47444b
renee <2560885w@student.gla.ac.uk>
2026-05-12 19:59:11 +0100

    changed sizing and padding

--- rent-calculator/src/app/page.tsx
+++ rent-calculator/src/app/page.tsx
@@ -7,7 +7,7 @@ import TabPanel from "../components/Tabs/TabPanel";
 
 export default function App() {
     return (
-        <div className="flex flex-col h-full w-full bg-gray-500 py-8 px-2">
+        <div className="flex flex-col min-h-screen min-w-screen w-fit py-2 px-2">
             <Header></Header>
             <TabsProvider>
                 <Tabs></Tabs>
