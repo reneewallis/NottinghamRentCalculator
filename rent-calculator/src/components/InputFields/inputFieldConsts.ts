726306c2ed26e49d558f967046366f9b02d67eb5
renee <2560885w@student.gla.ac.uk>
2026-05-14 20:22:12 +0100

    adjusted date box height to match textbox height

--- rent-calculator/src/components/InputFields/inputFieldConsts.ts
+++ rent-calculator/src/components/InputFields/inputFieldConsts.ts
@@ -1,5 +1,5 @@
 export const DATE_BOX_WIDTH = 13.536;
-export const DATE_BOX_HEIGHT = 3.45;
+export const DATE_BOX_HEIGHT = 3.449;
 export const DEFAULT_TEXT_BOX_WIDTH = 11.25;
 export const SIDE_PANEL_TEXT_BOX_WIDTH = 11.95;
 export const MIN_TEXT_BOX_WIDTH_SCALE = 0.65;
