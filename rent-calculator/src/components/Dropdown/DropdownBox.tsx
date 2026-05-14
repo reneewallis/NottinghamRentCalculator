04dea1e77fe70a593d9279f87ea06785b739b9ad
renee <2560885w@student.gla.ac.uk>
2026-05-14 19:50:40 +0100

    added responsive resizing of dropdown box and made default sizing smaller

--- rent-calculator/src/components/Dropdown/DropdownBox.tsx
+++ rent-calculator/src/components/Dropdown/DropdownBox.tsx
@@ -2,6 +2,8 @@
 
 import React, { useState } from "react";
 import { DropdownBoxProps } from "@/src/types/Dropdown";
+import { fluidCSSWidthScale } from "@/src/utils/HelperFunctions";
+import { DEFAULT_DROPDOWN_LABEL_SCALE, MAX_DROPDOWN_WIDTH_SCALE, MIN_DROPDOWN_WIDTH_SCALE, SMALL_DROPDOWN_LABEL_SCALE } from "./dropdownConstants";
 
 function CustomDropdownBox<TLabel extends string>(
     props: DropdownBoxProps<TLabel>,
@@ -30,27 +32,32 @@ function CustomDropdownBox<TLabel extends string>(
                   )
                 : label.length;
         if (!small) {
-            buttonWidth = maxLabelLength * 1.15;
+            buttonWidth = maxLabelLength * DEFAULT_DROPDOWN_LABEL_SCALE;
         } else {
-            buttonWidth = maxLabelLength * 0.7;
+            buttonWidth = maxLabelLength * SMALL_DROPDOWN_LABEL_SCALE;
         }
     }
+    const boxLabel = controlled ? props.value : boxText;
 
     return (
         <div className="relative inline-flex flex-col items-center">
             <button
-                style={{ width: `${buttonWidth}rem` }}
+                style={{ minWidth: fluidCSSWidthScale(`${buttonWidth * MIN_DROPDOWN_WIDTH_SCALE}rem`, `${buttonWidth}rem`, `${buttonWidth * MAX_DROPDOWN_WIDTH_SCALE}rem`) }}
                 key={`dropdown ${label}`}
                 className={`flex justify-between items-center ${small ? "py-3 gap-1" : "py-6 gap-2"} pl-4 border-2 rounded-3xl cursor-pointer transition-colors ${showItems ? `text-gray-100 font-semibold bg-gray-800 hover:bg-gray-900 hover:text-gray-50 ${valid ? "border-gray-100 hover:border-gray-50" : "border-red-500"}` : `text-gray-200 bg-gray-700 hover:bg-gray-800 hover:text-gray-100 ${valid ? "border-gray-300 hover:border-gray-200" : "border-red-600"}`}`}
                 onClick={() => setShowItems(!showItems)}
             >
                 <div
                     key={"boxLabel"}
-                    className={`${small ? "text-2xl wrap-break-word" : "text-3xl whitespace-nowrap"} text-left`}
+                    className={`${small ? "text-base md:text-lg lg:text-2xl wrap-break-word whitespace-normal" : "text-lg md:text-xl lg:text-3xl whitespace-nowrap"} text-left`}
                 >
-                    {controlled ? props.value : boxText}
+                    {small? boxLabel.split(" ").map((word,index) => (
+                        <span key={index} className="block">
+                            {word}
+                        </span>
+                    )): boxLabel}
                 </div>
-                <div key={"arrow"} className="translate-y-1 pr-1.5 mr-2.5">
+                <div key={"arrow"} className="translate-y-0.5 lg:translate-y-1 pr-1.5 mr-2.5">
                     {showItems ? (
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -58,7 +65,7 @@ function CustomDropdownBox<TLabel extends string>(
                             viewBox="0 0 24 24"
                             strokeWidth={3}
                             stroke="currentColor"
-                            className="size-6"
+                            className="size-4 md:size-5 lg:size-6"
                         >
                             <path
                                 strokeLinecap="round"
@@ -73,7 +80,7 @@ function CustomDropdownBox<TLabel extends string>(
                             viewBox="0 0 24 24"
                             strokeWidth={2}
                             stroke="currentColor"
-                            className="size-7"
+                            className="size-5 md:size-6 lg:size-7"
                         >
                             <path
                                 strokeLinecap="round"
@@ -89,7 +96,7 @@ function CustomDropdownBox<TLabel extends string>(
                     {items.map((item, index) => (
                         <button
                             key={`item${index}`}
-                            className="whitespace-nowrap text-xl pt-0.5 pb-1 pl-2 pr-2 text-left text-gray-100 rounded-2xl hover:bg-fuchsia-700 hover:text-gray-50 hover:font-semibold cursor-pointer"
+                            className="whitespace-nowrap text-base md:text-lg lg:text-xl pt-0.5 pb-1 pl-2 pr-2 text-left text-gray-100 rounded-2xl hover:bg-fuchsia-700 hover:text-gray-50 hover:font-semibold cursor-pointer"
                             onClick={() => {
                                 if (item.onClick) {
                                     item.onClick();
