360e1182013bcbd2aceeb2a81373d3b3d9879c57
renee <2560885w@student.gla.ac.uk>
2026-05-14 19:48:18 +0100

    Renamed file

--- rent-calculator/src/components/InputFields/inputFieldConsts.ts
+++ rent-calculator/src/components/InputFields/inputFieldConsts.ts
@@ -3,4 +3,4 @@ export const DATE_BOX_HEIGHT = 3.54;
 export const DEFAULT_TEXT_BOX_WIDTH = 11.25;
 export const SIDE_PANEL_TEXT_BOX_WIDTH = 11.95;
 export const MIN_TEXT_BOX_WIDTH_SCALE = 0.65;
-export const MAX_TEXT_BOX_WIDTH_SCALE = 1.25;
+export const MAX_TEXT_BOX_WIDTH_SCALE = 1.1;
