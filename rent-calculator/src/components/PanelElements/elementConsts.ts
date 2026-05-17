97d62a3a60823ce02639d2adf24bd79b32994b6c
renee <2560885w@student.gla.ac.uk>
2026-05-17 19:00:02 +0100

    Created consts for panel elements

---
+++ rent-calculator/src/components/PanelElements/elementConsts.ts
@@ -0,0 +1,8 @@
+export const ELEMENT_MIN_SCALE = 0.75;
+export const ELEMENT_MAX_SCALE = 1.1;
+export const DEFAULT_CIRCLE_SIZE = 16;
+export const CIRCLE_RIGHT_DISTANCE = 1.1975;
+export const MAIN_PANEL_WIDTH = 18.5;
+export const PANELS_MARGIN_TOP = 10;
+export const MAIN_PANEL_PADDING_TOP = 6.4375;
+export const SIDE_PANEL_PADDING_VISIBLE_SIDE = 1.75;
