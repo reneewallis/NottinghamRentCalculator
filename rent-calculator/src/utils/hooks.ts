48c857cd9edfc2870a3d7c898541aa4c68341f8a
renee <2560885w@student.gla.ac.uk>
2026-05-18 05:22:31 +0100

    Added useViewportWidth and useMaxTabs hooks

---
+++ rent-calculator/src/utils/hooks.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { TAB_BUTTONS_CONTAINER_WIDTH, TAB_CONTAINER_WIDTH } from "../components/Tabs/tabConsts";
+import { getMaxTabs } from "./helperFunctions";
+
+function useViewportWidthRem(){
+    const [width, setWidth] = useState(0);
+
+    useEffect(() => {
+        const updateWidth = () => {
+            const widthPx = document.documentElement.clientWidth;
+
+            const rootFontSize = parseFloat(getComputedStyle(document.documentElement).fontSize);
+
+            setWidth(widthPx / rootFontSize);
+        };
+
+        updateWidth();
+
+        const observer = new ResizeObserver(updateWidth);
+        observer.observe(document.documentElement);
+
+        return () => observer.disconnect();
+    }, []);
+
+    return width;
+}
+
+export function useMaxTabs(){
+    const viewportWidth = useViewportWidthRem();
+    return getMaxTabs(viewportWidth);
+}
