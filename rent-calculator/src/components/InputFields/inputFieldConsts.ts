4510cf82a043b26dc1177cdf5a1ba29e9b149486
renee <2560885w@student.gla.ac.uk>
2026-05-25 00:01:20 +0100

    increased min text box width scale

--- rent-calculator/src/components/InputFields/inputFieldConsts.ts
+++ rent-calculator/src/components/InputFields/inputFieldConsts.ts
@@ -2,5 +2,5 @@ export const DATE_BOX_WIDTH = 13.536;
 export const DATE_BOX_HEIGHT = 3.449;
 export const DEFAULT_TEXT_BOX_WIDTH = 11.25;
 export const SIDE_PANEL_TEXT_BOX_WIDTH = 11.95;
-export const MIN_TEXT_BOX_WIDTH_SCALE = 0.65;
+export const MIN_TEXT_BOX_WIDTH_SCALE = 0.75;
 export const MAX_TEXT_BOX_WIDTH_SCALE = 1.1;
